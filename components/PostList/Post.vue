ed1853016d1bfc3d587d2f114168685edd935281
adivank <adam.ivankovic@gmail.com>
2022-10-17 00:01:34 +0200

    initial post-list post syling

---
+++ components/PostList/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="post">
+    <div class="post__header">
+      <div class="post__image">
+        <img src="https://source.unsplash.com/random/100x100" alt="">
+      </div>
+      <div class="post__info">
+        <p class="post__author">Adam Ivanković</p>
+        <p class="post__time info">21. September 2022.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ components/PostList/PostList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="post-list">
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+  components: { Post }
+}
+</script>
